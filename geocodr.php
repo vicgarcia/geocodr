ce2ca79d285fe4d17ed1b8a0addcf6299dfa654a
Vic Garcia <vic.garcia@outlook.com>
2014-08-30 01:50:22 +0000

    remove token auth and to the bone functions

--- geocodr.php
+++ geocodr.php
@@ -5,41 +5,35 @@ require '../vendor/autoload.php';
 use \Slim;
 use \PDO;
 
-$tokens = require '../tokens.php';
-$connection = require '../connection.php';
+$config = require '../config.php';
 
-$query = preg_replace('/\s+/', ' ', "
-  SELECT
-    g.rating,
-    ST_X(g.geomout) As lon,
-    ST_Y(g.geomout) As lat,
-    (addy).address As stno,
-    (addy).streetname As street,
-    (addy).streettypeabbrev As styp,
-    (addy).location As city,
-    (addy).stateabbrev As st,
-    (addy).zip
-  FROM
-    geocode(:address) AS g;
-");
+$query = "
+    SELECT
+      g.rating,
+      ST_X(g.geomout) As lon,
+      ST_Y(g.geomout) As lat,
+      (addy).address As stno,
+      (addy).streetname As street,
+      (addy).streettypeabbrev As styp,
+      (addy).location As city,
+      (addy).stateabbrev As st,
+      (addy).zip
+    FROM
+      geocode(:address) AS g;
+    ";
 
 $app = new Slim\Slim();
 
-$app->post('/', function() use ($app, $tokens, $connection, $query) {
-    $token = $app->request()->params('token');
-    if (in_array($token, $tokens)) {
-        $db = new PDO($connection);
-        $stmt = $db->prepare($query);
-        $address = $app->request()->params('address');
-        $stmt->execute([':address' => $address]);
-        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$app->post('/', function() use ($app, $config, $query) {
+    $db = new PDO($config);
+    $stmt = $db->prepare($query);
 
-        $app->response->setStatus(200);
-        echo json_encode($results);
-    } else {
-        $app->response->setStatus(403);
-        echo 'invalid token';
-    }
+    $address = $app->request()->params('address');
+    $stmt->execute([':address' => $address]);
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $app->response->setStatus(200);
+    echo json_encode($results);
 });
 
 $app->notFound(function () use ($app) {
